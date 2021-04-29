b343290dc0a82b6d05065d4f7bd896908462c337
韓珮宜 <ei15200405@gmail.com>
2021-04-29 20:24:23 +0800

    change

---
+++ Web410637285/Controllers/Web410637285Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410637285.Controllers
+{
+    public class Web410637285Controller : Controller
+    {
+        // GET: Web410637285
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
